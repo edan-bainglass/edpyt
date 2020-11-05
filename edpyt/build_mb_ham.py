64526d9e84aa9760f8b5d116dc0617007cad9fc0
ggandus <ggandus@student.ethz.ch>
2020-11-05 11:04:28 +0100

    Pass many-body test

--- edpyt/build_mb_ham.py
+++ edpyt/build_mb_ham.py
@@ -3,7 +3,6 @@ from numba import njit
 
 # Sparse matrix format
 from scipy.sparse import (
-    csr_matrix
     coo_matrix
 )
 
@@ -22,8 +21,6 @@ from shared import (
 )
 
 from operators import (
-    c,
-    cdg,
     flip,
     fsgn
 )
@@ -39,46 +36,33 @@ Conventions:
     dwn : sectot number of down spin states (all possible permutations of ndw in n)
 """
 
+
 @njit
-def add_hoppings(ix_s, states, H, c, vals, rows, cols, nzi, nzj):
-    """Add hoppings from nonzero elements H[nzi, nzj]
-    for state index ix_s in sparse matrix csr(vals, (rows, cols)).
+def add_hoppings(ix_s, states, T_data, T_r, T_c, c, val, row, col):
+    """Add hoppings to many-body Hamiltonian.
+
+    Args:
+        ix_s : initial state (index) in `states`
+        states : spin states
+        T_data, T_r, T_r : hopping coo matrix vectors
+        c : sequential index in many-body vectors
+        val, row, col : many-body coo matrix vectors
 
     """
     s = states[ix_s]
 
-    # if nzi is None:
-    #     nzi, nzj = np.where(~np.eye(H.shape[0],dtype=bool))
-
-    # c = 0
-    # for i in nzi:
-    #     for j in nzj:
-    for i, j in zip(nzi, nzj):
+    for i, j, elem in zip(T_r, T_c, T_data):
         if ((s>>i)&unsiged_dt(1)) and (not (s>>j)&unsiged_dt(1)):
-            # fsgn_t, t = c(s, j)
             t = flip(s, j)
             sgn_t = fsgn(s, j)
             f = flip(t, i)
             sgn_f = fsgn(t, i)
-            # fsgn_f, sm = cdg(t, i)
-            vals[c] += H[i, j] * sgn_t * sgn_f
-            rows[c] += ix_s
-            cols[c] += np.searchsorted(states, f)
+            val[c] += elem * sgn_t * sgn_f
+            row[c] += ix_s
+            col[c] += np.searchsorted(states, f)
             c += 1
-            # if (not (s>>i)&unsiged_dt(1)) and ((s>>j)&unsiged_dt(1)):
-            #     # fsgn_t, t = c(s, j)
-            #     t = flip(s, i)
-            #     sgn_t = fsgn(s, i)
-            #     f = flip(t, j)
-            #     sgn_f = fsgn(t, j)
-            #     # fsgn_f, sm = cdg(t, i)
-            #     vals[c] = H[j, i] * sgn_t * sgn_f
-            #     cols[c] = ix_s
-            #     rows[c] = np.searchsorted(states, f)
-            #     c += 1
-
-    print(c)
     return c
+
 
 def build_mb_ham(H, V, nup, ndw):
     """Build sparse Hamiltonian of the sector.
@@ -99,7 +83,7 @@ def build_mb_ham(H, V, nup, ndw):
     dwn = states_dw.size
     d = dup * dwn
 
-    i_vals = np.zeros(d, dtype=np.float64)
+    vec_diag = np.zeros(d, dtype=np.float64)
 
     # On-site terms :
     for i in range(d):
@@ -110,45 +94,67 @@ def build_mb_ham(H, V, nup, ndw):
         sdw = states_dw[idw]
 
         # Singly-occupied : \sum_{l,sigma} n^+_{l,sigma}
-        onsite_energy = np.sum(H.diagonal() * binrep(sup^sdw, n))
+        onsite_energy = np.sum(H.diagonal() * binrep(sup, n))
+        onsite_energy += np.sum(H.diagonal() * binrep(sdw, n))
 
         # Doubly-occupied : \sum_{l,sigma} n^+_{l,sigma} n_{l,sigma'}
         onsite_int = np.sum(V.diagonal() * binrep(sup&sdw, n))
 
-        i_vals[i] = onsite_energy + onsite_int
+        vec_diag[i] = onsite_energy + onsite_int
 
-    nzi, nzj = np.nonzero(H)
-    neq = nzi != nzj
-    nzi = nzi[neq]
-    nzj = nzj[neq]
-    data = H[nzi, nzj]
-    H_coo_mat = coo_matrix((data, (nzi, nzj)), shape=(n,n))
+    # Hoppings
+    T = nnz_offdiag_coomat(H)
+    nnz_offdiag = len(T.data)
 
-    nnz_up_count = len(nzi) * int(binom(n-2, nup-1))
-    zeros = lambda dtype: np.zeros(nnz_up_count, dtype=dtype)
-    iup_coo_mat = coo_matrix(
-        (zeros(np.float64), (zeros(np.uint32), zeros(np.uint32)))
-        )
-    iup_vals = np.zeros(nnz_up_count, dtype=np.float64)
-    iup_rows = np.zeros(nnz_up_count, dtype=np.int32)
-    iup_cols = np.zeros(nnz_up_count, dtype=np.int32)
-    iup_coo_mat = coo_matrix((iup_vals))
+    nnz_up_count = nnz_offdiag * int(binom(n-2, nup-1))
+    sp_mat_up = empty_coomat(nnz_up_count, (dup, dup))
 
     c = 0
     for iup in range(dup):
-        c = add_hoppings(iup, states_up, H, c, iup_vals, iup_rows, iup_cols, nzi, nzj)
+        c = add_hoppings(iup, states_up,
+            # Hoppings
+            T.data, T.row, T.col,
+            # Sequential index in many-body nnz.
+            c,
+            # Many-Body Hamiltonian
+            sp_mat_up.data, sp_mat_up.row, sp_mat_up.col)
 
-    iup_mat = csr_matrix((iup_vals, (iup_rows, iup_cols)), shape=(dup, dup))
-
-    nnz_dw_count = len(nzi) * int(binom(n-2, ndw-1))
-    idw_vals = np.zeros(nnz_dw_count, dtype=np.float64)
-    idw_rows = np.zeros(nnz_dw_count, dtype=np.int32)
-    idw_cols = np.zeros(nnz_dw_count, dtype=np.int32)
+    nnz_dw_count = nnz_offdiag * int(binom(n-2, ndw-1))
+    sp_mat_dw = empty_coomat(nnz_dw_count, (dwn, dwn))
 
     c = 0
     for idw in range(dwn):
-        c = add_hoppings(idw, states_dw, H, c, idw_vals, idw_rows, idw_cols, nzi, nzj)
+        c = add_hoppings(idw, states_dw,
+            # Hoppings
+            T.data, T.row, T.col,
+            # Sequential index in many-body nnz.
+            c,
+            # Many-Body Hamiltonian
+            sp_mat_dw.data, sp_mat_dw.row, sp_mat_dw.col)
 
-    idw_mat = csr_matrix((idw_vals, (idw_rows, idw_cols)), shape=(dwn, dwn))
+    return vec_diag, sp_mat_up, sp_mat_dw
 
-    return i_vals, iup_mat, idw_mat
+
+
+def nnz_offdiag_coomat(mat):
+    """Return nonzero off-diagonal elements of matrix mat in
+    scipy::sparse::coo_matrix format.
+
+    """
+    nzi, nzj = np.nonzero(mat)
+    neq = nzi != nzj
+    nzi = nzi[neq]
+    nzj = nzj[neq]
+    data = mat[nzi, nzj]
+    return coo_matrix((data, (nzi, nzj)), shape=mat.shape)
+
+
+def empty_coomat(nnz_count, shape, data_dt=np.float64):
+    """Return scipy::sparse::coo_matrix with nnz_count elements.
+
+    """
+    zeros = lambda dtype: np.zeros(nnz_count, dtype=dtype)
+    rows = zeros(unsiged_dt)
+    cols = zeros(unsiged_dt)
+    data = zeros(data_dt)
+    return coo_matrix((data, (rows, cols)), shape=shape)
--- edpyt/test_build_mb_ham.py
+++ edpyt/test_build_mb_ham.py
@@ -5,21 +5,31 @@ from build_mb_ham import (
 )
 
 
+t12 = -1.
+t21 = -1.
+U1 = 1.
+U2 = 0.
+e1 = -0.5
+e2 = -0.25
+
+H = np.array([
+    [e1,t12],
+    [t21,e2]
+])
+
+V = np.array([
+    [U1,0],
+    [0,U2]
+])
+
 def test_build_mb_ham():
 
-    t = -1
+    # sz = 0 sector
 
-    H = np.array([
-        [0,t],
-        [t,0]
-    ])
+    nup = 1
+    ndw = 1
 
-    V = np.array([
-        [0,0],
-        [0,0]
-    ])
-
-    i_vals, iup_mat, idw_mat = build_mb_ham(H, V, 1, 1)
+    i_vals, iup_mat, idw_mat = build_mb_ham(H, V, nup, ndw)
 
     dup = iup_mat.shape[0]
     dwn = idw_mat.shape[0]
@@ -28,4 +38,96 @@ def test_build_mb_ham():
              + np.kron(iup_mat.todense(), np.eye(dwn)) \
              + np.kron(np.eye(dup), idw_mat.todense())
 
-    np.savetxt('mb_ham.txt', mb_ham)
+    # states :: 01, 10
+
+    #
+    # state :   |up,dw>   |up,dw>
+    # ----------------------------
+    #  s0   :     0,0      1,1
+    #  s1   :     1,0      0,1
+    #  s2   :     0,1      1,0
+    #  s3   :     1,1      0,0
+
+    # Hamiltonian
+
+    #           s0       s1      s2      s3
+    #
+    #   s0   2*e2+U2 | -t21  |  -t21  |   0
+    #        ---------------------------------
+    #   s1    -t12   | e1+e2 |    0   |  -t21
+    #        ---------------------------------
+    #   s2    -t12   |    0  |  e1+e2 |  -t21
+    #        ---------------------------------
+    #   s3      0    |  -t12 |   -t12  | 2*e1+U1
+
+    expected = np.array([
+        [2*e2+U2,-t21,-t21,0],
+        [-t12,e1+e2,0,-t21],
+        [-t12,0,e1+e2,-t21],
+        [0,-t12,-t12,2*e1+U1]
+    ])
+
+    assert np.allclose(mb_ham, expected)
+
+
+    # sz = 1 sector
+
+    nup = 2
+    ndw = 0
+
+    i_vals, iup_mat, idw_mat = build_mb_ham(H, V, nup, ndw)
+
+    dup = iup_mat.shape[0]
+    dwn = idw_mat.shape[0]
+
+    mb_ham = np.diag(i_vals) \
+             + np.kron(iup_mat.todense(), np.eye(dwn)) \
+             + np.kron(np.eye(dup), idw_mat.todense())
+
+    #
+    # state :   |up,dw>   |up,dw>
+    # ----------------------------
+    #  s0   :     1,0      1,0
+
+    # Hamiltonian
+
+    #           s0
+    #
+    #   s0   e1+e2
+
+    expected = np.array([
+        [e1+e2]
+    ])
+
+    assert np.allclose(mb_ham, expected)
+
+    # sz = -1 sector
+
+    nup = 0
+    ndw = 2
+
+    i_vals, iup_mat, idw_mat = build_mb_ham(H, V, nup, ndw)
+
+    dup = iup_mat.shape[0]
+    dwn = idw_mat.shape[0]
+
+    mb_ham = np.diag(i_vals) \
+             + np.kron(iup_mat.todense(), np.eye(dwn)) \
+             + np.kron(np.eye(dup), idw_mat.todense())
+
+    #
+    # state :   |up,dw>   |up,dw>
+    # ----------------------------
+    #  s0   :     0,1      0,1
+
+    # Hamiltonian
+
+    #           s0
+    #
+    #   s0   e1+e2
+
+    expected = np.array([
+        [e1+e2]
+    ])
+
+    assert np.allclose(mb_ham, expected)
