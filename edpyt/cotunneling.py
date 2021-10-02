fix: fermionic sign count occupied from left of position.

edpyt/cotunneling.py
@@ -67,9 +67,9 @@ def project_add(ispin_i, ispin_j, n, nupI, ndwI, espace):
     v_JI = np.empty((sctJ.eigvals.size,sctI.eigvals.size,n))
     v_FJ = np.empty((sctF.eigvals.size,sctJ.eigvals.size,n))
     for j in range(n):
-        v_JI[...,j] = sgnI * proj_ispin_j(j, cdg, sctI, sctJ)
+        v_JI[...,j] = sgnI * proj_ispin_j(j, n, cdg, sctI, sctJ)
     for i in range(n):
-        v_FJ[...,i] = sgnJ * proj_ispin_i(i, c, sctJ, sctF)
+        v_FJ[...,i] = sgnJ * proj_ispin_i(i, n, c, sctJ, sctF)
     dE = sctF.eigvals[:,None]-sctI.eigvals[None,:]
     E = sctJ.eigvals[None,:]-sctI.eigvals[:,None]
     gf2elist = []
@@ -103,9 +103,9 @@ def project_sub(ispin_i, ispin_j, n, nupI, ndwI, espace):
     v_JI = np.empty((sctJ.eigvals.size,sctI.eigvals.size,n))
     v_FJ = np.empty((sctF.eigvals.size,sctJ.eigvals.size,n))
     for j in range(n):
-        v_JI[...,j] = sgnI * proj_ispin_j(j, c, sctI, sctJ)
+        v_JI[...,j] = sgnI * proj_ispin_j(j, n, c, sctI, sctJ)
     for i in range(n):
-        v_FJ[...,i] = sgnJ * proj_ispin_i(i, cdg, sctJ, sctF)
+        v_FJ[...,i] = sgnJ * proj_ispin_i(i, n, cdg, sctJ, sctF)
     dE = sctF.eigvals[:,None]-sctI.eigvals[None,:]
     E = -sctJ.eigvals[None,:]+sctI.eigvals[:,None]
     gf2hlist = []

edpyt/gf_exact.py
@@ -39,7 +39,7 @@ class Gf:
         return res / self.Z
 
 
-def project_exact_up(pos, op, sctI, sctJ):
+def project_exact_up(pos, n, op, sctI, sctJ):
     """Project states of sector sctI onto eigenbasis of sector sctJ.
 
     """
@@ -55,14 +55,14 @@ def project_exact_up(pos, op, sctI, sctJ):
         supI = sctI.states.up[iupI]
         # Check for empty impurity
         if check_occupation(supI, pos):
-            sgnJ, supJ = op(supI, pos)
+            sgnJ, supJ = op(supI, pos, n)
             iupJ = binsearch(sctJ.states.up, supJ)
             v0 += np.float64(sgnJ)*sctJ.eigvecs[iupJ::sctJ.dup,:].T.dot(
                                    sctI.eigvecs[iupI::sctI.dup,:])
     return v0
 
 
-def project_exact_dw(pos, op, sctI, sctJ):
+def project_exact_dw(pos, n, op, sctI, sctJ):
     """Project states of sector sctI onto eigenbasis of sector sctJ.
 
     """
@@ -78,7 +78,7 @@ def project_exact_dw(pos, op, sctI, sctJ):
         sdwI = sctI.states.dw[idwI]
         # Check for empty impurity
         if check_occupation(sdwI, pos):
-            sgnJ, sdwJ = op(sdwI, pos)
+            sgnJ, sdwJ = op(sdwI, pos, n)
             idwJ = binsearch(sctJ.states.dw, sdwJ)
             v0 += np.float64(sgnJ)*sctJ.eigvecs[idwJ*sctJ.dup:(idwJ+1)*sctJ.dup,:].T.dot(
                                    sctI.eigvecs[idwI*sctI.dup:(idwI+1)*sctI.dup,:])
@@ -125,7 +125,7 @@ def build_gf_exact(H, V, espace, beta, egs=0., pos=0, ispin=0):
         EI = (sctI.eigvals-egs)[None,:]
         EJ = (sctJ.eigvals-egs)[:,None]
         exponents = np.exp(-beta*EJ) + np.exp(-beta*EI)
-        bJ = project_exact(pos, cdg, sctI, sctJ)
+        bJ = project_exact(pos, n, cdg, sctI, sctJ)
         lambdas.extend((EJ - EI).flatten())
         qs.extend((bJ**2 * exponents).flatten())
 

edpyt/gf_lanczos.py
@@ -52,7 +52,7 @@ class Gf:
         return out / self.Z
 
 
-def project_up(pos, op, sctI, sctJ):
+def project_up(pos, n, op, sctI, sctJ):
     """Project states of sector sctI onto eigenbasis of sector sctJ (nupJ=nupI+1).
 
     """
@@ -71,13 +71,13 @@ def project_up(pos, op, sctI, sctJ):
         supI = sctI.states.up[iupI]
         # Check for empty impurity
         if check_occupation(supI, pos):
-            sgnJ, supJ = op(supI, pos)
+            sgnJ, supJ = op(supI, pos, n)
             iupJ = binsearch(sctJ.states.up, supJ)
             v0[:,iupJ::sctJ.dup] = np.float64(sgnJ)*sctI.eigvecs[iupI::sctI.dup,:].T
     return v0
 
 
-def project_dw(pos, op, sctI, sctJ):
+def project_dw(pos, n, op, sctI, sctJ):
     """Project states of sector sctI onto eigenbasis of sector sctJ (ndwJ=ndwI+1).
 
     """
@@ -87,7 +87,7 @@ def project_dw(pos, op, sctI, sctJ):
         sdwI = sctI.states.dw[idwI]
         # Check for empty impurity
         if check_occupation(sdwI, pos):
-            sgnJ, sdwJ = op(sdwI, pos)
+            sgnJ, sdwJ = op(sdwI, pos, n)
             idwJ = binsearch(sctJ.states.dw, sdwJ)
             v0[:,idwJ*sctJ.dup:(idwJ+1)*sctJ.dup] = np.float64(sgnJ)*sctI.eigvecs[idwI*sctI.dup:(idwI+1)*sctI.dup,:].T
     return v0
@@ -186,7 +186,7 @@ def build_gf_lanczos(H, V, espace, beta, egs=0., pos=0, repr='cf', ispin=0, sepa
                 )
                 sctJ = build_from_sector(sctJ,eigvals=eigvals,eigvecs=eigvecs)
                 # <J|I>
-                bJ = project_exact(pos, cdg, sctI, sctJ)
+                bJ = project_exact(pos, n, cdg, sctI, sctJ)
                 # EJ-EI
                 l = sctJ.eigvals[:,None] - sctI.eigvals[None,:]
                 q = bJ**2 * exponents[None,:]
@@ -197,7 +197,7 @@ def build_gf_lanczos(H, V, espace, beta, egs=0., pos=0, repr='cf', ispin=0, sepa
             # solve with Lanczos
             else:
                 # <I|J>
-                v0 = project(pos, cdg, sctI, sctJ)
+                v0 = project(pos, n, cdg, sctI, sctJ)
                 matvec = matvec_operator(
                     *build_mb_ham(H, V, sctJ.states.up, sctJ.states.dw)
                 )
@@ -221,7 +221,7 @@ def build_gf_lanczos(H, V, espace, beta, egs=0., pos=0, repr='cf', ispin=0, sepa
                 )
                 sctJ = build_from_sector(sctJ,eigvals=eigvals,eigvecs=eigvecs)
                 # <J|I>
-                bJ = project_exact(pos, c, sctI, sctJ)
+                bJ = project_exact(pos, n, c, sctI, sctJ)
                 # EI-EJ
                 l = - sctJ.eigvals[:,None] + sctI.eigvals[None,:]
                 q = bJ**2 * exponents[None,:]
@@ -232,7 +232,7 @@ def build_gf_lanczos(H, V, espace, beta, egs=0., pos=0, repr='cf', ispin=0, sepa
             # solve with Lanczos
             else:
                 # <I|J>
-                v0 = project(pos, c, sctI, sctJ)
+                v0 = project(pos, n, c, sctI, sctJ)
                 matvec = matvec_operator(
                     *build_mb_ham(H, V, sctJ.states.up, sctJ.states.dw)
                 )

edpyt/sqtunneling.py
@@ -22,7 +22,7 @@ def project_add(ispin, n, nupI, ndwI, espace):
     project_exact = project_exact_up if ispin==0 else project_exact_dw
     v_JI = np.empty((sctJ.eigvals.size,sctI.eigvals.size,n))
     for i in range(n):
-        v_JI[...,i] = project_exact(i, cdg, sctI, sctJ)
+        v_JI[...,i] = project_exact(i, n, cdg, sctI, sctJ)
     E = sctJ.eigvals[:,None]-sctI.eigvals[None,:]
     return [Gfe((nupJ,ndwJ,j),(nupI,ndwI,i),v_JI[j,i],E[j,i]) for j,i in np.ndindex(v_JI.shape[:2])]
 
@@ -38,7 +38,7 @@ def project_sub(ispin, n, nupI, ndwI, espace):
     project_exact = project_exact_up if ispin==0 else project_exact_dw
     v_JI = np.empty((sctJ.eigvals.size,sctI.eigvals.size,n))
     for i in range(n):
-        v_JI[...,i] = project_exact(i, c, sctI, sctJ)
+        v_JI[...,i] = project_exact(i, n, c, sctI, sctJ)
     E = -sctJ.eigvals[:,None]+sctI.eigvals[None,:]
     return [Gfh((nupJ,ndwJ,j),(nupI,ndwI,i),v_JI[j,i],E[j,i]) for j,i in np.ndindex(v_JI.shape[:2])]
 

tests/test_operators.py
@@ -10,11 +10,12 @@ from edpyt.operators import (
 import numpy as np
 
 def test_fsgn():
-
-    s = int("1001101",base=2)
-    assert fsgn(s, np.int32(2)) == (-1)**1
-    assert fsgn(s, 3) == (-1)**2
-    assert fsgn(s, 6) == (-1)**3
+    sbin = "1001101"
+    n = len(sbin)
+    s = int(sbin,base=2)
+    assert fsgn(s, np.int32(2), n) == (-1)**2
+    assert fsgn(s, 3, n) == (-1)**1
+    assert fsgn(s, 6, n) == (-1)**0
 
 
 def test_flip():
@@ -25,33 +26,38 @@ def test_flip():
 
 
 def test_fer_algebra():
-
-    s = int("101101",base=2)
+    sbin = "101101"
+    n = len(sbin)
+    s = int(sbin,base=2)
     i = 0
     j = 1
     # {c_i,c^+_j} = c_i c^+_j + c^+_j c_i = \delta_ij
     #
-    tsgn, t = c(s, i)
-    fsgn, f1 = cdg(t, j)
+    tsgn, t = c(s, i, n)
+    fsgn, f1 = cdg(t, j, n)
     sgn1 = tsgn * fsgn
     #
-    tsgn, t = cdg(s, j)
-    fsgn, f2 = c(t, i)
+    tsgn, t = cdg(s, j, n)
+    fsgn, f2 = c(t, i, n)
     sgn2 = tsgn * fsgn
     #
     assert sgn1*f1 + sgn2*f2 == 0
 
 
 def test_cdg():
-    s = int("1001101",base=2)
-    sgn, f = cdg(s, 1)
-    assert sgn==-1
+    sbin = "1001101"
+    n = len(sbin)
+    s = int(sbin,base=2)
+    sgn, f = cdg(s, 1, n)
+    assert sgn==(-1)**3
     assert f==int("1001111",base=2)
 
 def test_c():
-    s = int("1001101",base=2)
-    sgn, f = c(s, 2)
-    assert sgn==-1
+    sbin = "1001101"
+    n = len(sbin)
+    s = int(sbin,base=2)
+    sgn, f = c(s, 2, n)
+    assert sgn==(-1)**2
     assert f==int("1001001",base=2)
 
 